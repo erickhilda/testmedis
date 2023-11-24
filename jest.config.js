5cb533f9fcbe8a1a0f48420b99085a284ff7f029
erickhilda <erickh.andreas@gmail.com>
2023-11-24 19:04:13 +0700

    init

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  clearMocks: true,
+  collectCoverage: true,
+  cpverageProvider: 'v8',
+  moduleFileExtensions: ['js', 'ts'],
+
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+};
